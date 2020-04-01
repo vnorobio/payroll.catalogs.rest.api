e4776a6ed0b848b9d073ed9bc8609fb212bf2aae
Victor Orobio <vnorobio@gmail.com>
2020-03-31 21:47:58 -0500

    basic structure

---
+++ src/test/java/com/clinic/payroll/catalogs/rest/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinic.payroll.catalogs.rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
